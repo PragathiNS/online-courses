a0fc063c4622e247702943d0c8586e019bca4d86
Pragathi <pragathi.code@gmail.com>
2018-03-22 20:46:47 -0700

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,17 @@
 filename = "words.txt"
+fhandle = open(filename)
 
 word_count = {}
-with open(filename):
-  line = 
+for line in fhandle:
+  words = line.split()
+  for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+  
+bigCount = 0
+bigWord = 0
+for word, count in word_count.items():
+  if bigCount is None or count > bigCount:
+    bigCount = count
+    bigWord = word
+
+print (bigWord, bigCount)
