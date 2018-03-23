8c63d26570220e5dd4bef8e004cf41dcb40201d5
Pragathi <pragathi.code@gmail.com>
2018-03-22 22:11:58 -0700

    Update top10_commonwords.py

--- top10_commonwords.py
+++ top10_commonwords.py
@@ -7,8 +7,8 @@ for line in fhandle:
   for word in words:
     counts[word] = counts.get(word, 0) + 1
   
-lst = []
-for k, v in count.items():
+"""lst = []
+for k, v in counts.items():
   new_tup = (v, k)
   lst.append(new_tup)
   
@@ -16,3 +16,6 @@ lst = sorted(lst, reverse=True)
 
 for v, k in lst[:10]:
   print (k, v)
+"""
+# Using List Comprehension
+print (sorted([(v,k) for k,v in counts.items()], reverse=True))
