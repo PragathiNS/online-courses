7f450cd6d8f805dcdee28d6f8f60d6ca01a19b6c
Pragathi NS <pragathi.code@gmail.com>
2018-03-24 05:17:07 -0700

    assignments from the coursera

--- Python/xmlparsing.py
+++ Python/xmlparsing.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 data = '''<person><name>Chuck</name><phone type='intl">+1 734 303 4456</phone><email hide="yes"/></person>'''
 
+# fromstring() method parses the xml stored in data
 tree = ET.fromstring(data)
 print('Name:', tree.find('name').text)
 print('Attr:', tree.find('email').get('hide'))
