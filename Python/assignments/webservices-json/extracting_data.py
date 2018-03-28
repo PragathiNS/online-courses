4da268657e5b3d2ba0f7cdbd0dd9ecdbe236b78f
Pragathi <pragathi.code@gmail.com>
2018-03-27 19:29:34 -0700

    Update extracting_data.py

--- Python/assignments/webservices-json/extracting_data.py
+++ Python/assignments/webservices-json/extracting_data.py
@@ -4,3 +4,22 @@
 # compute the sum of the numbers in the file.
 
 import urllib.request, urllib.parse, urllib.error
+import json
+
+url = input('Enter location: ')
+data = urllib.request.urlopen(url)
+dd = data.read()
+info = json.loads(dd)
+
+numbers = []
+count = 0
+for item in info['comments']:
+  numbers.append(int(item['count']))
+  count = count + 1
+  
+s = 0
+for i in numbers:
+  s = s + i
+  
+print ('Count:', count)
+print ('Sum:', s)
