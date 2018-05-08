1c8a883f1a1a80d13a4fc57870ad91aaa763fb19
Pragathi NS <pragathi.code@gmail.com>
2018-05-08 16:25:51 -0700

    csv parsing

--- DataWrangling_MongoDB/01_Lesson/csv_parsing.py
+++ DataWrangling_MongoDB/01_Lesson/csv_parsing.py
@@ -38,8 +38,10 @@ def test():
     # a simple test of your implemetation
     datafile = os.path.join(DATADIR, DATAFILE)
     d = parse_file(datafile)
-    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
-    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)',
+            'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 
+            'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
 
     assert d[0] == firstline
     assert d[9] == tenthline
---
+++ DataWrangling_MongoDB/01_Lesson/csv_parsing_mod.py
@@ -0,0 +1,21 @@
+import os
+import csv
+import pprint
+
+DATADIR = ""
+DATAFILE = "beatles-diskography.csv"
+
+
+def parse_file(datafile):
+    data = []
+    with open(datafile, "r") as f:
+        dd = csv.DictReader(f)
+        for line in dd:
+#            print (line)
+            data.append(line)
+    return data
+
+if __name__ == '__main__':
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    pprint.pprint(d)
